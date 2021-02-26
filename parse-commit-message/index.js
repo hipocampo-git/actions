f62e6221bc52d5f568da490f8005f64f71844c25
Oliver Pike <oliver@hipocampo.com>
2021-02-26 12:40:19 -0300

    changes

--- parse-commit-message/index.js
+++ parse-commit-message/index.js
@@ -4,20 +4,24 @@ const github = require("@actions/github");
 async function run() {
   try {
     const token = core.getInput("token");
-    const title = core.getInput("title");
-    const body = core.getInput("body");
-    const assignees = core.getInput("assignees");
+    const message = core.getInput("commit-message");
+    // const title = core.getInput("title");
+    // const body = core.getInput("body");
+    // const assignees = core.getInput("assignees");
 
     const octokit = github.getOctokit(token);
 
-    const response = await octokit.issues.create({
-      ...github.context.repo,
-      title,
-      body,
-      assignees: assignees ? assignees.split("\n") : undefined
-    });
+    // const response = await octokit.issues.create({
+    //   ...github.context.repo,
+    //   title,
+    //   body,
+    //   assignees: assignees ? assignees.split("\n") : undefined
+    // });
+    let begin = message.indexOf('(#') + '(#'.length;
+    let end = message.indexOf(')', begin);
+    const pullRequestId = message.substring(begin, end).trim();
 
-    core.setOutput("issue", JSON.stringify(response.data));
+    core.setOutput("pull-request-id", JSON.stringify(pullRequestId));
   } catch (error) {
     core.setFailed(error.message);
   }
