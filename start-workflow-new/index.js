f30c24d558c301b53c32d89135aaed2475cbf948
Oliver Pike <oliver@hipocampo.com>
2021-03-01 11:04:38 -0300

    changes

--- start-workflow-new/index.js
+++ start-workflow-new/index.js
@@ -91,26 +91,26 @@ core.group('Do something async', async () => {
 
         core.debug(readResponse);
 
-        if (readResponse.length === 0) {
-          console.log('Branch name not found, creating new ci entry.');
-          const query =
-              `INSERT INTO workflows
-               (branch, pull_request_id)
-               VALUES ("${branchNameOutput}", ${prIdOutput})`;
-
-          const [response] = await connection.execute(query);
-
-          insertId = response.insertId;
-        } else {
-          insertId = readResponse[0].id;
-          herokuAppName = readResponse[0].heroku_app;
-          // It's possible that we created the db record but failed prior to
-          // deploying heroku.
-          if (herokuAppName) {
-            status = 'existing';
-          }
-          console.log(`ci id ${insertId} found for branch ${branchNameOutput}`);
-        }
+        // if (readResponse.length === 0) {
+        //   console.log('Branch name not found, creating new ci entry.');
+        //   const query =
+        //       `INSERT INTO workflows
+        //        (branch, pull_request_id)
+        //        VALUES ("${branchNameOutput}", ${prIdOutput})`;
+        //
+        //   const [response] = await connection.execute(query);
+        //
+        //   insertId = response.insertId;
+        // } else {
+        //   insertId = readResponse[0].id;
+        //   herokuAppName = readResponse[0].heroku_app;
+        //   // It's possible that we created the db record but failed prior to
+        //   // deploying heroku.
+        //   if (herokuAppName) {
+        //     status = 'existing';
+        //   }
+        //   console.log(`ci id ${insertId} found for branch ${branchNameOutput}`);
+        // }
         break;
       // if push event, do y
       // PR # ==> extracted from commit message
