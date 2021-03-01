e2c936eacedfb77a12725397e68f80fe3598d77f
Oliver Pike <oliver@hipocampo.com>
2021-03-01 11:27:28 -0300

    changes

--- start-workflow-new/index.js
+++ start-workflow-new/index.js
@@ -85,15 +85,17 @@ const foo = core.group('Do something async', async () => {
 
         // connection.resolve(1);
 
-        connection.end();
 
-        // const readResponse = await core.group('Do something else async', async () => {
-        //   const [readResponse] = await connection.execute(readQuery);
-        //   return readResponse
-        //   core.debug(someVar2);
-        // });
-        //
-        // core.debug(readResponse);
+
+        const readResponse = await core.group('Do something else async', async () => {
+          const [readResponse] = await connection.execute(readQuery);
+          return readResponse
+          core.debug(someVar2);
+        });
+
+        core.debug(readResponse);
+
+        // connection.end();
 
         // if (readResponse.length === 0) {
         //   console.log('Branch name not found, creating new ci entry.');
