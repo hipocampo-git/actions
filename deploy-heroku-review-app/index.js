d82fc32cff522ea0390a3aebe8531d1d2cfed520
Oliver Pike <oliver@hipocampo.com>
2021-03-02 16:09:51 -0300

    changes

--- deploy-heroku-review-app/index.js
+++ deploy-heroku-review-app/index.js
@@ -1,18 +1,15 @@
 const { Toolkit } = require("actions-toolkit");
-// const core = require("@actions/core");
-
 const Heroku = require("heroku-client");
 const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
 
 /**
  * Sleep prior to calling a function.
  * @param {Function} fn function to call after sleeping
- * @param {integer} timeout sleep duration in ms
+ * @param {number} timeout sleep duration in ms
  * @return {Promise<unknown>}
  */
 const sleep = (fn, timeout) => {
   return new Promise((resolve) => {
-    // wait 3s before calling fn(par)
     setTimeout(() => resolve(fn()), timeout)
   })
 };
@@ -20,11 +17,6 @@ const sleep = (fn, timeout) => {
 // Run your GitHub Action!
 Toolkit.run(
   async (tools) => {
-    // tools.context.ref
-    // tools.context.sha
-    // tools.context.repo
-
-    // Required information
     const event = tools.context.event;
 
     let pr;
@@ -35,10 +27,7 @@ Toolkit.run(
     let repo_url;
     let repo_name;
     let owner;
-    // tools.log.debug("context info",
-    //   tools.context);
-    // tools.log.debug("payload info",
-    //     tools.context.payload);
+
     if (event !== 'push') {
        pr = tools.context.payload.pull_request;
        branch = pr.head.ref;
@@ -59,7 +48,7 @@ Toolkit.run(
     }
 
     // Note!! Make sure you use a personal access token and not the implicit
-    //        secrets.GITHUB_TOKEN
+    //        secrets.GITHUB_TOKEN.
     const github_pa_token = process.env.GITHUB_PA_TOKEN;
 
     const source_url = `https://${owner}:${github_pa_token}@api.github.com/repos/${owner}/${repo_name}/tarball/${branch}`;
@@ -79,16 +68,8 @@ Toolkit.run(
       owner
     });
 
-    let action = tools.context.payload.action;
-
     // Output value indicating if this was a new or existing deployment.
     let status = 'new';
-
-    // We can delete a review app without them being a collaborator
-    // as the only people that can close PRs are maintainers or the author
-    // HIPOCAMPO TODO: Need to put the clean-up logic back in, here or another
-    //                 action.
-    // if (action === "closed") {
 
     // Fetch all PRs
     tools.log.pending("Listing review apps");
@@ -101,7 +82,6 @@ Toolkit.run(
     let app = reviewApps.find((app) => app.pr_number == pr_number);
     if (!app) {
       tools.log.info(`Did not find review app for PR number ${pr_number}`);
-      // return;
     } else {
       tools.log.pending(`Deleting existing review app id ${app.id}`);
       await heroku.delete(`/review-apps/${app.id}`);
@@ -161,7 +141,6 @@ Toolkit.run(
 
         tools.log.debug('Response received', resp);
 
-        // if not pending, done = true;
         if (resp.status === 'deleting') {
           tools.log.debug("Waiting...");
           await sleep(checkDeleteStatus, 20000);
@@ -182,11 +161,6 @@ Toolkit.run(
       tools.log.debug("Review app deleted");
     }
 
-    tools.log.debug('HERE 111111');
-
-    //   return;
-    // }
-
     // Do they have the required permissions?
     let requiredCollaboratorPermission = process.env.COLLABORATOR_PERMISSION;
     if (requiredCollaboratorPermission) {
@@ -196,9 +170,6 @@ Toolkit.run(
     } else {
       requiredCollaboratorPermission = ["triage", "write", "maintain", "admin"];
     }
-
-    // const reviewAppLabelName =
-    //   process.env.REVIEW_APP_LABEL_NAME || "review-app";
 
     const perms = await tools.github.repos.getCollaboratorPermissionLevel({
       ...tools.context.repo,
@@ -211,27 +182,7 @@ Toolkit.run(
 
     tools.log.info(`User is a collaborator: ${perms.data.permission}`);
 
-    // let createReviewApp = false;
     let createReviewApp = true;
-
-    // if (["opened", "reopened", "synchronize"].indexOf(action) !== -1) {
-    //   tools.log.info("PR opened by collaborator");
-    //   createReviewApp = true;
-    //   await tools.github.issues.addLabels({
-    //     ...tools.context.repo,
-    //     labels: ["review-app"],
-    //     issue_number: pr_number,
-    //   });
-    // } else if (action === "labeled") {
-    //   const labelName = tools.context.payload.label.name;
-    //   tools.log.info(`${labelName} label was added by collaborator`);
-    //
-    //   if (labelName === reviewAppLabelName) {
-    //     createReviewApp = true;
-    //   } else {
-    //     tools.log.debug(`Unexpected label, not creating app: ${labelName}`);
-    //   }
-    // }
 
     if (createReviewApp) {
       // If it's a fork, creating the review app will fail as there are no secrets available
@@ -243,16 +194,6 @@ Toolkit.run(
         tools.log.success("Action complete");
         return;
       }
-
-      // try {
-      //   const resp = await heroku.request({
-      //     path: `/review-apps${id}`,
-      //     method: "DELETE"
-      //   });
-      // } catch (error) {
-      //   tools.log.debug('Delete failed', error);
-      //   tools.log.debug("Unable to delete existing review app");
-      // }
 
       // Otherwise we can complete it in this run
       let resp;
@@ -327,12 +268,6 @@ Toolkit.run(
         } else if (resp.status === 'created') {
           tools.outputs.status = status;
           tools.log.debug('outputs', tools.outputs);
-          // resp = await heroku.request({
-          //   path: `/apps/${reviewAppId}`,
-          //   method: "GET"
-          // });
-          // tools.log.debug('App info', resp);
-          // tools.outputs.review-app-name = resp.name;
 
           tools.log.success("Action complete");
         } else if (resp.status === 'errored') {
@@ -346,28 +281,7 @@ Toolkit.run(
       }
 
       await checkDeployStatus();
-
-      tools.log.debug('OFP - Exiting');
-
-      // let done = false;
-      // while (! done) {
-      //   // Check review app status
-      //   tools.log.debug("Checking deployment status...");
-      //   resp = await heroku.request({
-      //     path: `/review-apps/${reviewAppId}`,
-      //     method: "GET"
-      //   });
-      //
-      //   // if not pending, done = true;
-      //   if (resp.status !== 'pending') {
-      //     done = true;
-      //   }
-      // }
     }
-
-    // print(f"::set-output name=review_app_name::{review_app_name}")
-    // tools.outputs.status = status;
-    // tools.log.success("Action complete");
   },
   {
     event: [
--- start-workflow-new/index.js
+++ start-workflow-new/index.js
@@ -1,42 +1,21 @@
-// const { Toolkit } = require("actions-toolkit");
 const core = require("@actions/core");
 const github = require("@actions/github");
 const mysqlPromise = require('mysql2/promise');
 
-// async function run() {
-const foo = core.group('Do something async', async () => {
+core.group('Doing something async', async () => {
   let connection = null;
   try {
-    core.startGroup("Logging context object");
-    console.log(JSON.stringify(github.context, null, "\t"));
-    core.endGroup();
-
-    core.startGroup("Logging payload object");
-    console.log(JSON.stringify(github.context.payload, null, "\t"));
-    core.endGroup();
-
-
     core.startGroup("env variables");
     console.log(JSON.stringify(process.env, null, "\t"));
     core.endGroup();
 
-    const token = core.getInput("token");
-    // const commitMessage = core.getInput("commit-message");
-    // const eventName = core.getInput("event-name");
     const eventName = github.context.eventName;
-    const prId = core.getInput("pull-request-id");
-    const actionName = core.getInput("action-name");
-    // const title = core.getInput("title");
-    // const body = core.getInput("body");
-    // const assignees = core.getInput("assignees");
-
-    const octokit = github.getOctokit(token);
 
     let prIdOutput = '';
     let herokuAppOutput = '';
     let branchNameOutput = '';
     let instanceNameOutput = '';
-    // let ciIdOutput = '';
+
     const herokuAppPrefix = 'hipocampo-pr-';
     const instancePrefix = 'hipocampo-test-ci-';
 
@@ -59,30 +38,18 @@ const foo = core.group('Do something async', async () => {
       return `SELECT * FROM workflows WHERE pull_request_id=${prId}`;
     };
 
-    // let readQuery =
-    // `SELECT * FROM workflows WHERE branch="${branchNameOutput}"`;
-
     switch (eventName) {
-      // if pull request event, do x
-      // BRANCH NAME ==> ${GITHUB_HEAD_REF}
-      // PR # ==> github.event.pull_request.number
-      // HEROKU APP ==> hipocampo-pr- + PR #
-      // if activity == "opened" ==> write into workflows table
       case 'pull_request':
-        // let pr = tools.context.payload.pull_request;
         branchNameOutput =  github.context.payload.pull_request.head.ref;
         prIdOutput = github.context.payload.number;
         herokuAppOutput = herokuAppPrefix + prIdOutput;
         instanceNameOutput = instancePrefix + prIdOutput;
 
-        // let insertId = null;
         let status = 'new';
         let herokuAppName = null;
 
         const [readResponse] =
             await connection.execute(readQueryTemplate(prIdOutput));
-
-        core.debug(readResponse);
 
         if (readResponse.length === 0) {
           console.log('pull request id not found, creating new ci entry.');
@@ -92,29 +59,17 @@ const foo = core.group('Do something async', async () => {
                VALUES ("${branchNameOutput}", ${prIdOutput}, "${herokuAppOutput}", "${instanceNameOutput}")`;
 
           const [response] = await connection.execute(query);
-
-          // ciIdOutput = response.insertId;
         } else {
-          // ciIdOutput = readResponse[0].id;
-          // herokuAppName = readResponse[0].heroku_app;
-          // instanceNameOutput = readResponse[0].database_name;
-          // prIdOutput =
           // It's possible that we created the db record but failed prior to
           // deploying heroku.
           if (herokuAppName) {
+            // We currently aren't doing anything with these status values.
+            // TODO: Either remove them or make use of them.
             status = 'existing';
           }
-          // console.log(
-          //     `ci id ${ciIdOutput} found for branch ${branchNameOutput}`);
         }
         break;
-      // if push event, do y
-      // PR # ==> extracted from commit message
-      // BRANCH NAME ==> do lookup in workflows table based of PR #
-      // HEROKU APP ==> do lookup in workflows table based of PR #
       case 'push':
-        // const commits = github.context.payload.commits;
-
         const commitMessage = github.context.payload.head_commit.message;
         let begin = commitMessage.indexOf('(#') + '(#'.length;
         let end = commitMessage.indexOf(')', begin);
@@ -145,40 +100,25 @@ const foo = core.group('Do something async', async () => {
           branchNameOutput = readResponse2[0].branch;
         }
         break;
-      // if workflow dispatch event do z
-      //  BRANCH NAME ==> ${GITHUB_REF#refs/heads/}
-      // PR # ==> either ${{ github.event.number}} or do a lookup
-      // HEROKU APP ==> hipocampo-pr- + PR # (or do a lookup)
       case 'default':
         // Default is workflow dispatch right now
         // TODO: figure out the specific string for workflow dispatch events.
         break;
     }
 
-    // const response = await octokit.issues.create({
-    //   ...github.context.repo,
-    //   title,
-    //   body,
-    //   assignees: assignees ? assignees.split("\n") : undefined
-    // });
-
-    // TODO: Not sure if we need to output this still.
     core.setOutput("pull-request-id", prIdOutput);
     core.setOutput("heroku-app-name", herokuAppOutput);
     core.setOutput("branch-name", branchNameOutput);
     core.setOutput("instance-name", instanceNameOutput);
-    // connection.end();
   } catch (error) {
     core.setFailed(error.message);
     core.setOutput("pull-request-id", "something");
   } finally {
     if (connection) {
-      // The github action won't terminate without this.
+      // NOTE: The github action won't terminate without this line.
       connection.end();
     }
   }
 });
 
 return;
-
-// foo.resolve(1);
