8cd1713705cd1a4d76d76aef07d772e33a203846
Oliver Pike <oliver@hipocampo.com>
2021-03-28 10:18:54 -0300

    changes

--- teardown/index.js
+++ teardown/index.js
@@ -22,7 +22,7 @@ core.group('Doing something async', async () => {
     });
     const client = await auth.getClient();
     console.log('HERE 10');
-    const instances = sqlAdmin.instances.list();
+    const instances = sqlAdmin.instances.list('bitcoin-core-test');
     console.log(instances);
 
     // Delete all databases
